e721f0a594b1d738072c09e4c226f31786e6c5fa
rufus0077 <rufusdavid007@gmail.com>
2024-10-14 18:53:42 +0530

    changes made

--- JS -oop/oop.js
+++ JS -oop/oop.js
@@ -18,32 +18,67 @@
 
 //*********************************************************************************** */
 
-// factory re constructor function 
+// factory re constructor function (return)
 
-function createCircle(radius){
-    return { // return indicates factory function
-        radius,
-        draw : function(){
-            console.log('draw');
-        }
+// function createCircle(radius){
+//     return { // return indicates factory function
+//         radius,
+//         draw : function(){
+//             console.log('draw');
+//         }
 
-    }
-}
+//     }
+// }
 
-const cirCle =createCircle(1);
+// const cirCle =createCircle(1);
 // console.log(cirCle);
 
 // /********************************************************************************************************************//
 
-// Constructor
+// Constructor(this) Built in constructor function
 
-function CirCle(radius){   // this is a type of class in js when we use capital letters in the function name
-    this.radius = radius;
-    this.draw = function(){
-        console.log("draw");
-    }
+// function CirCle(radius){   // this is a type of class in js when we use capital letters in the function name
+//     this.radius = radius;
+//     this.draw = function(){
+//         console.log("draw");
+//     }
 
-}
+// }
 
-const another = new CirCle(1);// imagine it creates new object in heap with different address to it
-console.log(another);
+// const another = new CirCle(1);// imagine it creates new object in heap with different address to it
+// // if we forget to use new keyword it will assig to the global object
+// console.log(another);
+
+// *************************************************************************************************************************
+
+// value types and reference types
+
+//Value types
+// two are different and stores values seperately
+
+// Primitives are copied by their value 
+
+
+// let x=10; 
+// let y =x;
+
+// x=20;
+
+// console.log(x);
+// console.log(y);
+
+// *******************************************************************************
+
+
+// Objects are copied by their reference 
+
+// let x={value: 10}; 
+// let y =x;
+
+// x.value=20;
+
+// console.log(x);
+// console.log(y);
+
+
+// **********************************************************************************
