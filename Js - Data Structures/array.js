8a124a4eddb2cb3dc781afde1a5367b04e9f298b
Rufus C <rufusc2004@gmail.com>
2024-10-15 19:26:34 +0530

    added Ds

---
+++ Js - Data Structures/array.js
@@ -0,0 +1,8 @@
+// rm -f .git/index.lock to save from index lock or git lock
+
+
+// java script i s not tightly coupled arrays can store both int and string or an diff datatype in an single array 
+
+let colors =['blue', 'orange'];
+colors[2] = 1;
+console.log(colors.length);
